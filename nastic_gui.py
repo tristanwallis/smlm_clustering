caa9a2430a3c4b17fde4055336388e0d28a84e34
Tristan Wallis <81597257+tristanwallis@users.noreply.github.com>
2021-12-06 10:54:18 +1000

    Update nastic_gui.py

--- nastic_gui.py
+++ nastic_gui.py
@@ -30,7 +30,7 @@ This script has been tested and will run as intended on Windows 7/10, with minor
 The script will fork to multiple CPU cores for the heavy number crunching routines (this also prevents it from being packaged as an exe using pyinstaller).
 Feedback, suggestions and improvements are welcome. Sanctimonious critiques on the pythonic inelegance of the coding are not.
 '''
-last_changed = "20211118"
+last_changed = "20211206"
 
 # MULTIPROCESSING FUNCTIONS
 from scipy.spatial import ConvexHull
